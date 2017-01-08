3daefbd5ec654cb93cfb971ac700b51bc70c70b4
AlabamaBen <benjamin.b.morelle@gmail.com>
2017-01-08 13:21:30 +0100

    Update Pawn.cpp and Pawn.hpp
    Implement the Check function for pawn

--- Pawn.cpp
+++ Pawn.cpp
@@ -4,56 +4,80 @@
 
 Pawn::Pawn(bool isBlack, Plat* pl) : Piece("Pawn", 'p', isBlack, pl) {}
 
+bool Pawn::hasntMoved(const ChessAction &a)const
+{
+	std::pair<int, int> from = convertS2P(a.from);
+	Plat::Pie_ptr MovingPiece = plate->getPieceAt(a.from);
+	if (MovingPiece->isBlack)
+	{
+		return from.second == 7;
+	}
+	else
+	{
+		return from.second == 2;
+	}
+	return false;
+}
+
 bool Pawn::check(const ChessAction &a) const {
-  return true ;
-  /*
 	std::pair<int, int> from = convertS2P(a.from);
 	std::pair<int, int> to = convertS2P(a.to);
-
-	Plat::Pie_ptr MovingPiece = plate->getPieceAt(a.from);
-	Plat::Pie_ptr ToEatPiece;
-
-	int dx = from.first - to.first;
-	int dy = from.second - to.second;
 	
-	if (plate->getPieceAt(a.to)) 
+	Plat::Pie_ptr MovingPiece = plate->getPieceAt(a.from);
+
+	int dx = to.first - from.first ;
+	int dy = to.second - from.second;
+
+	if (plate->getPieceAt(a.to))
 	{
-		ToEatPiece = plate->getPieceAt(a.to);
+		Plat::Pie_ptr ToEatPiece = plate->getPieceAt(a.to);
 
 		if (MovingPiece->isBlack)
 		{
-			if (!ToEatPiece->isBlack && abs(dx) == 1 && dy == 1)
+			if (!ToEatPiece->isBlack && abs(dx) == 1 && dy == -1)
 			{
+				// std::cout<< "1";
 				return true;
+				
 			}
+			// std::cout<< "2\n";
+			return false;
 		}
 		else
 		{
-			if (ToEatPiece->isBlack && abs(dx) == 1 && dy == -1)
+			if (ToEatPiece->isBlack && abs(dx) == 1 && dy == 1)
 			{
+				// std::cout<< "3\n";
 				return true;
 			}
+			// std::cout<< "4\n";
+			return false;
 		}
 	}
 	else
 	{
 		if (MovingPiece->isBlack)
 		{
-			if (dy == -1)
+			if (this->hasntMoved(a))
 			{
-				return true;
+				// std::cout<< "5\n";
+				return ((dy == -1) || (dy == -2))&&(dx==0);
 			}
-			return false;
+			// std::cout<< "6\n";
+			return dy == -1;
 		}
 		else
 		{
-			if (dy == 1)
+			if (this->hasntMoved(a))
 			{
-				return true;
+				// std::cout<< "7\n";
+				return ((dy == 1) || (dy == 2))&&(dx==0);
 			}
-			return false;
+			// std::cout<< "8\n";
+			return dy == 1;
 		}
 	}
-  return false;*/
+	// std::cout<< "9\n";
+	return false;
 }
 Pawn::~Pawn() {}
--- Pawn.hpp
+++ Pawn.hpp
@@ -4,6 +4,7 @@
 
 class Pawn : public Piece {
 public:
+	bool hasntMoved(const ChessAction &a)const;
 	Pawn(bool isBlack, Plat* pl);
 	virtual bool check(const ChessAction &a) const;
 	virtual ~Pawn();
